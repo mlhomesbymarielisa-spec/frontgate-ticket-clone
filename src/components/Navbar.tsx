6f7362ef7771126cf33ef8d2d30209330e26d195
mlhomesbymarielisa-spec <mlhomesbymarielisa@gmail.com>
2026-03-29 02:12:26 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,8 +11,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <Link href="/" className="text-2xl font-black tracking-tighter text-white">
-              TICKET<span className="text-blue-500">VERSE</span>
+        
             </Link>
           </div>
           
