8999b6d3667305634e0530977009ef8879ff496b
mlhomesbymarielisa-spec <mlhomesbymarielisa@gmail.com>
2026-03-31 23:10:11 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,16 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Navbar from "@/components/Navbar"; // 1. Add this import
-
-export const metadata: Metadata = {
-  title: "Front Gate Tickets | Discover Your Next Experience",
-  description: "Redefining the festival ticketing experience for promoters & fans through innovation, creativity, & technology. ",
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
+import { ClerkProvider } from '@clerk/nextjs'
+import './globals.css'
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body className="antialiased bg-slate-950">
-        <Navbar /> {/* 2. Add the Navbar here */}
-        {children}
-      </body>
-    </html>
-  );
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
 }
